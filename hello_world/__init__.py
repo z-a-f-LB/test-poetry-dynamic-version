2e1e5f71859f6f3a024b0edec5749a8de7fd505b
Zafar <zafar@landabase.com>
2025-04-01 15:58:30 -0700

    Adding test builds in ci

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -5,7 +5,7 @@ Hello World package for cloud builds and git hooks.
 try:
     from ._version import __version__
 except ImportError:
-    __version__ = "0.0.0+placeholder"
+    __version__ = "0.0.0"
 
 
 def say_hello(name: str = "World") -> str:
