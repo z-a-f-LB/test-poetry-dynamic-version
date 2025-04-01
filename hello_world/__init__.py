b07c25910b369decc2503f85bac9775f0caea0b4
Zafar <zafar@landabase.com>
2025-04-01 14:48:24 -0700

    Remove version

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -3,3 +3,10 @@ Hello World package for cloud builds and git hooks.
 """
 
 __version__ = "0.0.0"
+
+
+def say_hello(name: str = "World") -> str:
+    """
+    Return a greeting message.
+    """
+    return f"Hello, {name}!"
