68346e5f65e67ba7ed5092004fa7b0eacf64a853
Zafar <zafar@landabase.com>
2025-04-02 10:11:59 -0700

    SHowcasing custom version locations

--- hello_world/_version.py
+++ hello_world/_version.py
@@ -1,3 +1,4 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0"
-__version_tuple__ = (0, 0, 0)
+# Any changes to this file will be lost.
+__version__ = "0.0.3+3"
+__version_tuple__ = (0, 0, 3, 3)
---
+++ hello_world/another_submodule.py
@@ -0,0 +1,3 @@
+"""Just to showcase that this file will be modified after poetry build"""
+
+__version__ = "0.0.0"
